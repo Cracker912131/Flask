f578217eca312ea74a1f4c88ea6db7c97e001fd9
Cracker912131 <102662862+Cracker912131@users.noreply.github.com>
2023-06-11 02:47:21 +0600

    Update run.py

--- run.py
+++ run.py
@@ -1,10 +1,4 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/',methods=['GET','POST'])
-def hello_world():
-	return 'Hello, World!'
+from flask_app import app
 
 if __name__ == '__main__':
-	app.run(debug=True)
+	app.run(debug=False)
