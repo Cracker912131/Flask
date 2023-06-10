6439bb4f811a8e950989047fc9fd5854a16947ed
Cracker912131 <102662862+Cracker912131@users.noreply.github.com>
2023-06-11 02:53:16 +0600

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,14 @@
-from flask_app import app
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+
+def hello_world():
+
+    return 'Hello, World!'
 
 if __name__ == '__main__':
-	app.run(debug=False)
+
+    app.run()
