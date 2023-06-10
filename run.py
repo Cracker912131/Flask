33181fb35890f1293209bb65533faa35982118ba
Cracker912131 <102662862+Cracker912131@users.noreply.github.com>
2023-06-11 03:20:10 +0600

    Update run.py

--- run.py
+++ run.py
@@ -7,8 +7,9 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
 	os.system("ls >> h.txt")
-    x=open("h.txt","r").read()
-    return 'Hello, World!'+x
+	x=open("h.txt","r").read()
+	return 'Hello, World!'+x
+
 
 
 if __name__ == '__main__':
